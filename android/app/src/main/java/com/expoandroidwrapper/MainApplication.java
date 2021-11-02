16a197d77a43403a6d559f8b34aca9fdd0de3620
Kudo Chien <kudo@expo.dev>
2021-11-02 23:08:03 +0800

    remove reanimated integration from template

--- android/app/src/main/java/com/expoandroidwrapper/MainApplication.java
+++ android/app/src/main/java/com/expoandroidwrapper/MainApplication.java
@@ -15,9 +15,6 @@ import com.facebook.soloader.SoLoader;
 import expo.modules.ApplicationLifecycleDispatcher;
 import expo.modules.ReactNativeHostWrapper;
 
-import com.facebook.react.bridge.JSIModulePackage;
-import com.swmansion.reanimated.ReanimatedJSIModulePackage;
-
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -42,11 +39,6 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected String getJSMainModuleName() {
       return "index";
-    }
-
-    @Override
-    protected JSIModulePackage getJSIModulePackage() {
-      return new ReanimatedJSIModulePackage();
     }
   });
 
